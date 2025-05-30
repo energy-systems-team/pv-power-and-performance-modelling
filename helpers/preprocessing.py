cfc08a9d275d024c6e7f0439e5d3b9fb6c6e834d
ppkart <karttunen.p.lauri@gmail.com>
2025-05-30 11:59:07 +0300

    Add a function for imputing data to columns with less frequent measuring resolution.

--- helpers/preprocessing.py
+++ helpers/preprocessing.py
@@ -2,6 +2,7 @@
 This file contains functions for pre-processing data.
 
 Original author: Väinö Anttalainen.
+Other editors: Lauri Karttunen
 """
 
 import pandas as pd
@@ -28,3 +29,31 @@ def merge_measurements(df, col_names, diff=10):
             measurements_df[['measurement_1', 'measurement_2']].mean(axis=1), np.nan)
 
     return measurements_df['avg']
+
+
+def impute_columns(df, col_names, resolutions):
+        """
+        Function for imputing missing values due to the different measurement resolutions.
+        :param df:
+        :param col_names: List of column names that are imputed.
+        :param resolution: List of measurement resolutions for the given columns in minutes.
+        :return: The modified df.
+        """
+        if len(col_names) != len(resolutions):
+                print("Length of col_names and resolutions must match. Columns not imputed.")
+        else:
+                num_of_rows_to_impute_list =  [res - 1 for res in resolutions]
+                for i in range(len(col_names)):
+                        num_of_rows_to_impute = num_of_rows_to_impute_list[i]
+                        column = col_names[i]
+                        values = df[column].values
+                        non_na_indices = np.where(~pd.isna(values))[0]
+                        ind_prev = 0
+                        for ind in non_na_indices:
+                                start_ind = max(0, ind - num_of_rows_to_impute) 
+                                if ind - ind_prev >= num_of_rows_to_impute:
+                                        values[start_ind:ind] = values[ind]
+                                ind_prev = ind
+                        
+                        df[column] = values
+        return df
